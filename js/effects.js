4930679e1ad74b15aae0b9b73507a83f8a949fa0
Ivan Voronov <10raven11@gmail.com>
2023-06-09 13:17:07 +0300

    Effect Slider

--- js/effects.js
+++ js/effects.js
@@ -7,18 +7,17 @@ const phobosButton = document.getElementById('effect-phobos');
 const heatButton = document.getElementById('effect-heat');
 const buttonArray = new Array (defaultButton, chromeButton, sepiaButton, marvinButton, phobosButton, heatButton);
 
-let currentEffect = 'none';
+const sliderElement = document.querySelector('.effect-level__slider');
+const valueElement = document.querySelector('.effect-level__value');
+
+let filterEffect = 'none';
+let postfix = '';
 
 const eventListeners = new Array(6);
 
 function generateEventListener(button) {
-  const newEffect = `effects__preview--${button.value}`;
   const eventListener = function() {
-    image.classList.remove(currentEffect);
-    if (button.value !== 'none') {
-      image.classList.add(newEffect);
-    }
-    currentEffect = newEffect;
+    updateEffect(button.value);
   };
   return eventListener;
 }
@@ -27,15 +26,113 @@ function disassembleEffects() {
   for(let i = 0; i < buttonArray.length; i++) {
     buttonArray[i].removeEventListener('click', eventListeners[i]);
   }
-  image.classList.remove(currentEffect);
+  sliderElement.noUiSlider.destroy();
 }
 
 function setupEffects() {
+  createSliderElement();
   for(let i = 0; i < buttonArray.length; i++) {
     eventListeners[i] = generateEventListener(buttonArray[i]);
     buttonArray[i].addEventListener('click', eventListeners[i]);
   }
   return disassembleEffects;
+}
+
+function updateEffect(type) {
+  let min = 0;
+  let max = 0;
+  let step = 0;
+  switch(type) {
+    case 'none':
+      filterEffect = 'none';
+      min = 1;
+      max = 100;
+      step = 1;
+      postfix = '';
+      break;
+    case 'chrome':
+      filterEffect = 'grayscale';
+      min = 0;
+      max = 1;
+      step = 0.1;
+      postfix = '';
+      break;
+    case 'sepia':
+      filterEffect = 'sepia';
+      min = 0;
+      max = 1;
+      step = 0.1;
+      postfix = '';
+      break;
+    case 'marvin':
+      filterEffect = 'invert';
+      min = 0;
+      max = 100;
+      step = 1;
+      postfix = '%';
+      break;
+    case 'phobos':
+      filterEffect = 'blur';
+      min = 0;
+      max = 3;
+      step = 0.1;
+      postfix = 'px';
+      break;
+    case 'heat':
+      filterEffect = 'brightness';
+      min = 1;
+      max = 3;
+      step = 0.1;
+      postfix = '';
+      break;
+    default:
+      filterEffect = 'none';
+      min = 1;
+      max = 100;
+      step = 1;
+      postfix = '';
+  }
+
+  if (filterEffect === 'none') {
+    sliderElement.setAttribute('disabled', true);
+  } else {
+    sliderElement.removeAttribute('disabled');
+  }
+  updateSlider(min, max, step);
+}
+
+function updateSlider(min, max, step) {
+  sliderElement.noUiSlider.updateOptions({
+    range: {
+      min: min,
+      max: max,
+    },
+    start: max,
+    step: step,
+  });
+}
+
+function createSliderElement() {
+  noUiSlider.create(sliderElement, {
+    range: {
+      min: 0,
+      max: 100,
+    },
+    start: 100,
+    step: 1,
+    connect: 'lower',
+  });
+
+  sliderElement.noUiSlider.on('update', () => {
+    valueElement.value = sliderElement.noUiSlider.get();
+    applyEffect(valueElement.value);
+  });
+  sliderElement.setAttribute('disabled', true);
+}
+
+function applyEffect(value) {
+  const filterString = filterEffect === 'none'? 'none': `${filterEffect}(${value}${postfix})`;
+  image.style.filter = filterString;
 }
 
 export {setupEffects};
